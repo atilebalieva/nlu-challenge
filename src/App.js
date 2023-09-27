8f0254d56b11a2000718c950e60c652c2cb8222a
atilebalieva <aitilebalieva@gmail.com>
2023-09-26 22:02:20 -0500

    Added navigation.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { Link } from 'react-router-dom';
 import './App.css';
+import logo from "./images/logo.png";
 
 function App() {
   return (
     <div>
-      
+            <header className="header">
+        <Link to="">
+          {" "}
+          <img src={logo} alt="Logo" />
+        </Link>
+        <nav className="navigation">
+          <Link to="/capabilities" className="link">
+            Capabilities
+          </Link>
+          <Link to="/flavors" className="link">
+            Flavors
+          </Link>
+          <Link to="/about-us" className="link">
+            About Us
+          </Link>
+          <Link to="/contact-us" className="link">
+            Contact Us
+          </Link>
+        </nav>
+      </header>
     </div>
   );
 }
