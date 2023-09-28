efd2ba425302e933984c800e6bb7420c5d947f3b
atilebalieva <aitilebalieva@gmail.com>
2023-09-28 00:15:20 -0500

    Updated slider responsive styles. Changed routes in App.js.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navigation from './Components/Navigation/Navigation';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/HomePage/Home";
+import Capabilities from "./Components/Capabilities/Capabilities";
+import Flavors from "./Components/Flavors/Flavors";
+import AboutUs from "./Components/AboutUs";
+import ContactUs from "./Components/ContactUs";
 import Footer from "./Components/Footer/Footer";
-import Flavors from './Components/Flavors/Flavors';
-import Home from './Components/HomePage/Home';
-import Capabilities from './Components/Capabilities/Capabilities';
-import Design from './Components/Capabilities/Design';
-import Production from './Components/Capabilities/Production';
-import Certification from './Components/Capabilities/Certification';
-import AboutUs from './Components/AboutUs';
-import ContactUs from './Components/AboutUs';
-import FlavorsList from './Components/Flavors/FlavorsList';
-import CompanyInfo from './Components/CompanyInfo/CompanyInfo'
+import Certification from "./Components/Capabilities/Certification";
+import Production from "./Components/Capabilities/Production";
+import Design from "./Components/Capabilities/Design";
+import FlavorsList from "./Components/Flavors/FlavorsList";
+import Navigation from "./Components/Navigation/Navigation";
+import CompanyInfo from "./Components/CompanyInfo/CompanyInfo";
+import "./css/media.css";
 
 function App() {
   return (
     <div className="main_container">
       <CompanyInfo/>
-     <Navigation/>
+   <Navigation/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/capabilities" element={<Capabilities />}></Route>
-        <Route path="/certification" element={<Certification/>}></Route>
-        <Route path="/production" element={<Production/>}></Route>
-        <Route path="/design" element={<Design/>}></Route>
         <Route path="/flavors" element={<Flavors />}></Route>
         <Route path='/flavors/:id' element={<FlavorsList/>}></Route>
         <Route path="/about-us" element={<AboutUs />}></Route>
         <Route path="/contact-us" element={<ContactUs />}></Route>
+        <Route path="/capabilities/certification" element={<Certification/>}></Route>
+        <Route path="/capabilities/production" element={<Production/>}></Route>
+        <Route path="/capabilities/design" element={<Design/>}></Route>
       </Routes>
       <Footer/>
     </div>
